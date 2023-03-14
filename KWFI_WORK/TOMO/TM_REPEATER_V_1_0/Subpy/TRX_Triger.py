973cb0bd621c592fdcf76d86d7a48916493ccc12
JinSeong <91999882+KangJinSeong@users.noreply.github.com>
2023-03-14 19:06:53 +0900

    Add files via upload

--- KWFI_WORK/TOMO/TM_REPEATER_V_1_0/Subpy/TRX_Triger.py
+++ KWFI_WORK/TOMO/TM_REPEATER_V_1_0/Subpy/TRX_Triger.py
@@ -6,27 +6,54 @@ By: Kang Jin seong
 
 import RPi.GPIO as GPIO
 import time
-
+from datetime import datetime
 
 class TRX_TRG:
     def __init__(self):
+        
         self.TXEN = 22
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.TXEN, GPIO.OUT)
         GPIO.output(self.TXEN, False)
         GPIO.setwarnings(False)
 
+        self.s_hour = 0
+        self.s_min = 0
+        self.s_interval = 0
+        self.s_id = 0
+
+        '''
+        DSP 통신 관련 초기화 함수 작성
+        '''
+    def timezone(self,hour,min,interval,id):
+        self.s_hour = hour
+        self.s_min = min
+        self.s_interval = interval
+        self.s_id = id
     def main(self):
-        GPIO.output(self.TXEN, True)
-        time.sleep(0.2)
-        GPIO.output(self.TXEN, False)
+        if ((self.s_hour+12)%24) == datetime.now().hour:
+            if ((self.s_min+self.s_interval)%60) == datetime.now().minute:
+                self.s_min = datetime.now().minute
+                '''
+                DSP 통신을 통한 TXID RS232 통신 코드 작성
+                '''
+                print('TRX_Triger START')
+                print(datetime.now().hour, datetime.now().minute)
+                time.sleep(3)
+                GPIO.output(self.TXEN, True)
+                time.sleep(0.2)
+                GPIO.output(self.TXEN, False)
 
 
 
 if __name__ == "__main__":
-    print('TRX_Triger Start')
+    print('TRX_Triger ON')
     A = TRX_TRG()
     try:
+        A.timezone(hour = 11,min = 57,interval = 1,id = 1,state = 1)
         A.main()
+ 
+        A.timezone(hour = 11,min = 57,interval = 2,id = 1,state = 1)
+
     except Exception as e:
         pass           
--- KWFI_WORK/TOMO/TM_REPEATER_V_1_0/main.py
+++ KWFI_WORK/TOMO/TM_REPEATER_V_1_0/main.py
@@ -1,7 +1,54 @@
 from Subpy import GPS_V_1_0, TRX_Controller_Swtich, TRX_Triger #하드웨어 조작 관련 모듈
+import RPi.GPIO as GPIO
+import serial
+from multiprocessing import Process, Queue
+import paho.mqtt.subscribe as subcribe
+import paho.mqtt.publish as publish
+import time
 
+class TM_Reapter:
+    def __init__(self):
+        self.Triger = TRX_Triger.TRX_TRG()
+        self.compas = GPS_V_1_0.GPS_HAT()
+        self.switch = TRX_Controller_Swtich.TRX_Power_switch()
+        self.TIMEDATA = Queue()
+
+        self.test_topics = 'Core/ttt'
+        self.broker ='test.mosquitto.org'
+
+    def core1(self):    # FPGA Triger 신호 생성 함수
+        while True:
+            if not self.TIMEDATA.empty():    #큐버퍼가 차있으면 실행
+                time_data = self.TIMEDATA.get()
+                td = time_data.split(',')
+                self.Triger.timezone(hour=int(td[0]),min=int(td[1]),interval=int(td[2]),id=int(td[3]))
+            self.Triger.main()  # Main 코드 완성해야함
+
+    def core2(self):
+        try:
+            while True:       
+                m = subcribe.simple(self.test_topics, hostname=self.broker,keepalive = 0)
+                print(m.payload)
+        except Exception as e:
+            pass
+
+
+
+    def core3(self):
+        pass
+
+    def core4(self):
+        pass    
 
 if __name__ == "__main__":
-    compas = GPS_V_1_0.GPS_HAT()
-    lat, lon = compas.main()
+    
+    A = TM_Reapter()
+    print('Detecting')
+
+    p1 = Process(target = A.core1, args=() )
+    p2 = Process(target = A.core2, args=() )
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
     
