734aab125cd43da9b33e56b94e718e07d0848ccd
JinSeong <91999882+KangJinSeong@users.noreply.github.com>
2023-03-16 10:18:21 +0900

    Add files via upload

--- KWFI_WORK/TOMO/TM_REPEATER_V_1_0/Subpy/TRX_Triger.py
+++ KWFI_WORK/TOMO/TM_REPEATER_V_1_0/Subpy/TRX_Triger.py
@@ -21,7 +21,6 @@ class TRX_TRG:
         self.s_min = 0
         self.s_interval = 0
         self.s_id = 0
-
         '''
         DSP 통신 관련 초기화 함수 작성
         '''
@@ -30,19 +29,38 @@ class TRX_TRG:
         self.s_min = min
         self.s_interval = interval
         self.s_id = id
+
+    def Data_analysis(self, data):  # data: 문자열 Mqtt로부터 수신 받은 데이터
+        Data = data.split(',')
+        print(Data)
+
+        if ('START' in Data):
+            result = [int(i) for i in Data[1:-1]]
+        else:
+            result = [24,60,60,100]
+        return result 
+
     def main(self):
-        if ((self.s_hour+12)%24) == datetime.now().hour:
-            if ((self.s_min+self.s_interval)%60) == datetime.now().minute:
-                self.s_min = datetime.now().minute
-                '''
-                DSP 통신을 통한 TXID RS232 통신 코드 작성
-                '''
-                print('TRX_Triger START')
-                print(datetime.now().hour, datetime.now().minute)
-                time.sleep(3)
-                GPIO.output(self.TXEN, True)
-                time.sleep(0.2)
-                GPIO.output(self.TXEN, False)
+        while True:
+            if (self.s_hour%24) == datetime.now().hour:
+                if (self.s_min%60) == datetime.now().minute:
+                    print(self.s_hour, self.s_min, self.s_interval, self.s_id)
+                    if self.s_min + self.s_interval == 60:
+                        self.s_hour = self.s_hour + 1
+                    
+                    self.s_min = datetime.now().minute
+                    print(self.s_hour, self.s_min, self.s_interval, self.s_id)
+                    '''
+                    DSP 통신을 통한 TXID RS232 통신 코드 작성
+                    '''
+                    print('TRX_Triger START')
+                    print(datetime.now().hour, datetime.now().minute)
+                    time.sleep(3)
+                    GPIO.output(self.TXEN, True)
+                    time.sleep(0.2)
+                    GPIO.output(self.TXEN, False)
+                    
+                    break
 
 
 
--- KWFI_WORK/TOMO/TM_REPEATER_V_1_0/main.py
+++ KWFI_WORK/TOMO/TM_REPEATER_V_1_0/main.py
@@ -1,3 +1,9 @@
+'''
+Date: 2023.03.16
+Title: 3차원해수유동 시간의 따른 TM_REPEATER SW REV(3)
+By: Kang Jin seong
+'''
+
 from Subpy import GPS_V_1_0, TRX_Controller_Swtich, TRX_Triger #하드웨어 조작 관련 모듈
 import RPi.GPIO as GPIO
 import serial
@@ -13,26 +19,27 @@ class TM_Reapter:
         self.switch = TRX_Controller_Swtich.TRX_Power_switch()
         self.TIMEDATA = Queue()
 
-        self.test_topics = 'Core/ttt'
+        self.test_topics = 'Core/test12343Demension/data'
         self.broker ='test.mosquitto.org'
 
     def core1(self):    # FPGA Triger 신호 생성 함수
         while True:
             if not self.TIMEDATA.empty():    #큐버퍼가 차있으면 실행
                 time_data = self.TIMEDATA.get()
-                td = time_data.split(',')
-                self.Triger.timezone(hour=int(td[0]),min=int(td[1]),interval=int(td[2]),id=int(td[3]))
-            self.Triger.main()  # Main 코드 완성해야함
+                answer = self.Triger.Data_analysis(time_data)
+                print(answer)
+                if sum(answer) < 100:
+                    self.Triger.timezone(hour=answer[0],min=answer[1],interval=answer[2],id=answer[3])
+                    self.Triger.main()  # Main 코드 완성해야함
 
     def core2(self):
         try:
             while True:       
                 m = subcribe.simple(self.test_topics, hostname=self.broker,keepalive = 0)
-                print(m.payload)
+                print(m.payload.decode())
+                self.TIMEDATA.put(m.payload.decode())
         except Exception as e:
             pass
-
-
 
     def core3(self):
         pass
