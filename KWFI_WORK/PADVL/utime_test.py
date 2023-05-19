ed5f330f56223f66a74cd1574d8e1cf8e69abdf6
JinSeong <91999882+KangJinSeong@users.noreply.github.com>
2023-05-19 12:18:14 +0900

    Delete KWFI_WORK directory

--- KWFI_WORK/PADVL/utime_test.py
+++
@@ -1,8 +0,0 @@
-import time
-
-usleep = lambda x: time.sleep(x/1000000.0)
-
-a = time.time()
-usleep(1)
-b = time.time()
-print(b-a)
--- KWFI_WORK/TOMO/GPS/serial_test.py
+++
@@ -1,4 +0,0 @@
-import serial
-ser = serial.Serial("/dev/ttyS0",baudrate = 9600)
-
-print('done')
